d4b556537447f76207516f7f2df5e12fa8d4b398
djvalente@gmail.com <djvalente@gmail.com>
2023-03-11 14:01:15 +0530

    pretty json

--- ASAv Interfaces.py
+++ ASAv Interfaces.py
@@ -14,9 +14,16 @@ def get_interfaces_physical(server, username, password):
     basicauth = (username, password)
 
     r = requests.get(url, auth=basicauth, headers=headers, verify=False)
-    print (r.status_code)
-    return r.content
+    
+    if (r.status_code == 200):
+        return r.content
+    else:
+        print ("ERROS Calling the API with code: " + str(r.status_code))
 
-# call the function to create an authentication token
+# call the function to get all physical interfaces
 int_list = get_interfaces_physical(server, username, password)
-print (int_list)
+
+if (int_list): # Only prints if the response as content
+    obj = json.loads(int_list)
+    print (json.dumps(obj,indent=4))  # Using loads/dumps just to print python indented
+
