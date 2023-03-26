cc286d3efdf3357866d1134463c41ec971ad728b
djvalente <djvalente@gmail.com>
2023-03-26 11:29:07 +0100

    netconf 01

---
+++ netconf01.py
@@ -0,0 +1,18 @@
+from ncclient import manager
+from xml.dom.minidom import parseString
+
+# Create an SSH Connection on port 830
+
+conn = manager.connect(
+    host = "10.255.1.102",
+    port = "830",
+    username = "admin",
+    password = "cisco",
+    hostkey_verify = False
+)
+
+print ("\n NETCONF Connection Established")
+
+# Imprimir a running config em XML
+netconf_reply = conn.get_config(source="running")
+print( parseString(netconf_reply.xml).toprettyxml() )
